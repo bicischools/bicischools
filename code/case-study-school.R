8af44334d5686faa93939550aa3a8e911829aef9
joeytalbot <52210872+joeytalbot@users.noreply.github.com>
2024-07-18 14:49:16 +0100

    case

---
+++ code/case-study-school.R
@@ -0,0 +1,12 @@
+home = readRDS("../internal/Bicischools_home_sample.Rds")
+school = readRDS("../internal/Bicischools_school_sample.Rds")
+
+tm_shape(home) + tm_lines()
+
+tm_shape(school) + tm_dots()
+
+home_dots = st_as_sf(home$home_coords)
+tm_shape(home_dots) + tm_dots()
+
+dim(home_dots)
+# [1] 169   3
