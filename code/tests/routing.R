72af81e6cf3241babf83ba23fd7818a788f4ccac
Joey Talbot <joeytalbot85@gmail.com>
2024-09-19 14:59:55 +0100

    all schools

--- code/tests/routing.R
+++ code/tests/routing.R
@@ -4,7 +4,10 @@ library(tmap)
 tmap_mode("view")
 
 res_constrained = readRDS("data/res_constrained.Rds")
-schools_c1 = readRDS("data/lisbon_c1_public.Rds")
+schools_c1 = readRDS("data/lisbon_c1.Rds")
+
+pal = c('#882255','#CC6677', '#44AA99', '#117733')
+quietness_breaks = c(0, 25, 50, 75, 100)
 
 summary(res_constrained$trips)
 larger_flows = res_constrained |> 
@@ -28,5 +31,13 @@ saveRDS(quiet_routes, "data/routes-quiet.Rds")
 # How to change the network to exclude forest paths?
 # osmextract::oe_get()
 
-tm_shape(quiet_routes) + tm_lines(lwd = "trips", col = "quietness", palette = "viridis") +
+
+tm_shape(quiet_routes) + tm_lines(lwd = "trips", 
+                                  col = "quietness", 
+                                  palette = pal,
+                                  breaks = quietness_breaks) +
+  tm_add_legend('fill', 
+                col = pal,
+                labels = c("0 to 25", "25 to 50", "50 to 75", "75 to 100"),
+                title="Tranquilidade") +
   tm_shape(schools_c1) + tm_dots()
