2f550f00539aa8b3eded8775fe3d1b1bfcda59b6
joeytalbot <joeytalbot85@gmail.com>
2025-03-11 12:43:38 +0000

    costa caparica

---
+++ code/costa-caparica.R
@@ -0,0 +1,23 @@
+library(sf)
+library(tmap)
+tmap_mode
+library(dplyr)
+
+res_output = read_sf("res_output_almada.geojson")
+
+schools_c1 = readRDS("data/c1.Rds")
+schools_c1 = schools_c1 |>
+  filter(MUNICIPIO == "Almada")
+
+schools_c1$case_study = grepl("Escola Básica n.º 2 da Costa da Caparica, Almada", schools_c1$ESCOLA)
+case_study = schools_c1[which(schools_c1$case_study),]
+
+res_cc = res_output |> 
+  filter(
+    D == case_study$DGEEC_id,
+    !is.na(trips_modelled)
+    )
+
+tm_shape(res_cc) + tm_lines()
+
+# Why is res_output$trips_modelled always NA?
