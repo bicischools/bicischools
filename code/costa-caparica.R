7ea9655a3cef0d86f3824b191313dd5c6c44c5ed
Joey Talbot <joeytalbot85@gmail.com>
2025-03-13 12:19:20 +0000

    bgri centroids

--- code/costa-caparica.R
+++ code/costa-caparica.R
@@ -14,6 +14,26 @@ schools_c1 = schools_c1 |>
 schools_c1$case_study = grepl("Escola Básica n.º 2 da Costa da Caparica, Almada", schools_c1$ESCOLA)
 case_study = schools_c1[which(schools_c1$case_study),]
 
+bgri = read_sf("../internal/BGRI21_170/BGRI21_170.gpkg")
+bgri_4326 = st_transform(bgri, 4326)
+bgri_4326 = st_make_valid(bgri_4326)
+
+bgri_almada = bgri_4326 |> 
+  filter(DTMN21 == 1503)
+
+zone_counts = bgri_almada |> 
+  select(OBJECTID, DTMN21, N_INDIVIDUOS_0_14)
+zone_centroids = st_centroid(zone_counts)
+
+# Zone centroids within 5km of school
+zone_centroids = zone_centroids |> 
+  mutate(
+    desire_line_length = st_distance(geom, case_study)[,1],
+    desire_line_length = units::drop_units(desire_line_length)
+  )
+centroids_5km = zone_centroids |> 
+  filter(desire_line_length < 5000)
+
 # Get OD desire lines for Costa da Caparica school
 res_cc = res_output |> 
   filter(
@@ -75,7 +95,6 @@ weightedMedian(route_summaries_all_fast$length, route_summaries_all_fast$trips_m
 # [1] 785.9941
 
 # median desire line length for all students living within 5km euclidean distance of the school
-library(matrixStats)
 weightedMedian(res_cc$desire_line_length, res_cc$trips_modelled)
 # [1] 564.7516
 
@@ -133,17 +152,17 @@ m10 = tm_shape(rnet_fast |> rename(`Cycle friendliness` = quietness)) +
 
 summary(routes_quiet$length)
 # Min. 1st Qu.  Median    Mean 3rd Qu.    Max. 
-# 144    1890    3024    2909    3856    4967 
+# 102    1127    2062    2292    3418    4829  
 summary(routes_fast$length)
 # Min. 1st Qu.  Median    Mean 3rd Qu.    Max. 
-# 144    1496    2525    2680    3637    4918 
+# 102     965    1824    2217    3704    4982 
 
 
 # Total number of students cycling to school under Go Dutch
 for(plan in plans) {
   routes_plan_pct = get(paste0("routes_", plan, "_pct"))
   route_summaries = routes_plan_pct |> 
-    group_by(OBJECTID,
+    group_by(O,
              route_number, 
              trips_modelled, 
              bicycle_godutch, 
@@ -154,23 +173,23 @@ for(plan in plans) {
   assign(paste0("route_summaries_", plan), route_summaries)
 }
 
-quiet_join = route_summaries_quiet |> 
-  sf::st_drop_geometry() |> 
-  select(OBJECTID, route_number, bicycle_godutch)
-centroids_quiet_5km = inner_join(centroids_5km, quiet_join, by = "OBJECTID")
+quiet_join = route_summaries_quiet |>
+  sf::st_drop_geometry() |>
+  select(O, route_number, bicycle_godutch)
+centroids_quiet_5km = inner_join(centroids_5km, quiet_join, by = c("OBJECTID" = "O"))
 
-fast_join = route_summaries_fast |> 
-  sf::st_drop_geometry() |> 
-  select(OBJECTID, route_number, bicycle_godutch)
-centroids_fast_5km = inner_join(centroids_5km, fast_join, by = "OBJECTID")
+fast_join = route_summaries_fast |>
+  sf::st_drop_geometry() |>
+  select(O, route_number, bicycle_godutch)
+centroids_fast_5km = inner_join(centroids_5km, fast_join, by = c("OBJECTID" = "O"))
 
 
 m0 = tm_shape(zone_centroids |> rename(`Number of students` = trips_modelled)) + tm_bubbles("Number of students", alpha = 0.3) +
   tm_shape(school) + tm_bubbles(col = "green")
-m1 = tm_shape(centroids_quiet_5km |> rename(`'Go Dutch' cycling potential` = bicycle_godutch)) + tm_bubbles("'Go Dutch' cycling potential", alpha = 0.3) + 
+m1 = tm_shape(centroids_quiet_5km |> rename(`'Go Dutch' cycling potential` = bicycle_godutch)) + tm_bubbles("'Go Dutch' cycling potential", alpha = 0.3) +
   tm_shape(school) + tm_bubbles(col = "green") +
   tm_shape(quiet_few) + tm_lines(lwd = 2)
-m5 = tm_shape(centroids_fast_5km |> rename(`'Go Dutch' cycling potential` = bicycle_godutch)) + tm_bubbles("'Go Dutch' cycling potential", alpha = 0.3) + 
+m5 = tm_shape(centroids_fast_5km |> rename(`'Go Dutch' cycling potential` = bicycle_godutch)) + tm_bubbles("'Go Dutch' cycling potential", alpha = 0.3) +
   tm_shape(school) + tm_bubbles(col = "green") +
   tm_shape(fast_few) + tm_lines(lwd = 2)
 
@@ -185,4 +204,141 @@ sum(route_summaries_quiet$bicycle_godutch)
 # [1] 40.38902
 sum(route_summaries_fast$bicycle_godutch)
 # [1] 40.47256
+
+
+# Bundling function -------------------------------------------------------
+
+# set of routes + (distance weighted number of cyclists on adjoining routes * route length within cropped zone) + minimum threshold 
+# buffer identifies when route segments have the same geometry
+
+rnet = rnet_quiet
+routes = route_summaries_quiet
+attribute_trips = "bicycle_godutch"
+min_trips = 3
+cycle_bus_routes = function(
+    routes,
+    rnet,
+    min_trips = 3,
+    attribute_trips = "bicycle_godutch",
+    buffer = 10
+) {
+  i = 1 # for testing
+  rnet_subset = rnet[rnet[[attribute_trips]] > min_trips,]
+  rnet_subset$length = sf::st_length(rnet_subset) |>
+    as.numeric()
+  rnet_union = sf::st_union(rnet_subset)
+  rnet_buffer = geo_buffer(rnet_union, dist = buffer)
+  routes_crop = sf::st_intersection(routes, rnet_buffer)
+  
+  routes_subset = routes_crop |> 
+    distinct(geometry)
+  routes_subset$id = seq.int(nrow(routes_subset))
+  routes_subset$length = sf::st_length(routes_subset) |> 
+    as.numeric()
+  attribute_trips_x_distance = paste0(attribute_trips, "_x_distance")
+  routes_subset[[attribute_trips_x_distance]] = NA
+  class(routes_subset[[attribute_trips_x_distance]]) = "numeric"
+  for (i in 1:nrow(routes_subset)) {
+    route_i = routes_subset[i,]
+    route_i_buffer = geo_buffer(route_i, dist = buffer)
+    rnet_in_route = rnet_subset[route_i_buffer, , op = sf::st_within]
+    routes_subset[i, attribute_trips_x_distance] = weighted.mean(rnet_in_route[[attribute_trips]], rnet_in_route$length)*routes_subset[i,]$length
+  }
+  routes_subset = routes_subset[order(routes_subset[[attribute_trips_x_distance]]),]
+  routes_subset
+}
+
+
+# Get all routes within min_trips rnet, ordered by length*mean_godutch
+ordered_routes_quiet = cycle_bus_routes(route_summaries_quiet, rnet_quiet, min_trips = 3, attribute_trips = "bicycle_godutch", buffer = 10)
+ordered_routes_fast = cycle_bus_routes(route_summaries_fast, rnet_fast, min_trips = 3, attribute_trips = "bicycle_godutch", buffer = 10)
+
+tm_shape(ordered_routes_quiet) + tm_lines()
+tm_shape(ordered_routes_fast) + tm_lines()
+
+# now identify which centroid OBJECTIDs are associated with which selected routes
+buffer = 10
+for(plan in plans) {
+  rnet_plan = get(paste0("rnet_", plan))
+  routes = get(paste0("route_summaries_", plan))
+  
+  rnet_subset = rnet_plan[rnet_plan[[attribute_trips]] > min_trips,]
+  rnet_subset$length = sf::st_length(rnet_subset) |>
+    as.numeric()
+  rnet_union = sf::st_union(rnet_subset)
+  rnet_buffer = geo_buffer(rnet_union, dist = buffer)
+  routes_crop = sf::st_intersection(routes, rnet_buffer)
+  
+  ordered_routes = get(paste0("ordered_routes_", plan))
+  join = sf::st_join(routes_crop, ordered_routes, join = st_equals)
+  route_stats = join |> 
+    mutate(full_length = length.x,
+           bike_bus_length = length.y,
+           dist_to_bike_bus = length.x - length.y) |> 
+    select(-length.x, -length.y)
+  assign(paste0("route_stats_", plan), route_stats)
+}
+
+
+rnet_quiet_subset = rnet_quiet[rnet_quiet[[attribute_trips]] > min_trips,]
+m3 = tm_shape(rnet_quiet_subset |> rename(`Potential cyclists` = bicycle_godutch)) +
+  tm_lines("Potential cyclists", palette = "viridis", lwd = 2, breaks = c(3, 5, 10, 100))
+
+rnet_fast_subset = rnet_fast[rnet_fast[[attribute_trips]] > min_trips,]
+m7 = tm_shape(rnet_fast_subset |> rename(`Potential cyclists` = bicycle_godutch)) +
+  tm_lines("Potential cyclists", palette = "viridis", lwd = 2, breaks = c(3, 5, 10, 100))
+
+# Remove routes with start points too close to other higher ranked routes
+
+routes = ordered_routes_quiet
+selected_routes = nrow(routes)
+filter_routes = function(
+    routes,
+    buffer,
+    top_n
+) {
+  i = 1
+  z = nrow(routes)-1
+  for(i in 1:z) {
+    route_i = routes[i, ]
+    p = st_cast(route_i$geometry, "POINT")
+    p1 = p[1]
+    better_routes = routes |> 
+      slice_tail(n = nrow(routes)-i)
+    better_routes_union = sf::st_union(better_routes)
+    distance = units::drop_units(sf::st_distance(p1, better_routes_union))
+    if(distance > buffer) {
+      selected_routes = c(selected_routes, i)
+    }
+    selected_routes
+  }
+  routes_subset = routes[selected_routes, ]
+  routes_subset = routes_subset[order(-routes_subset[["bicycle_godutch_x_distance"]]),]
+  routes_subset = routes_subset |> 
+    slice_head(n = top_n)
+  routes_subset
+}
+
+top_routes_quiet = filter_routes(routes = ordered_routes_quiet, buffer = 300, top_n = 3)
+top_routes_fast = filter_routes(routes = ordered_routes_fast, buffer = 300, top_n = 3)
+
+# Bubbles by number of students
+tm_shape(top_routes_quiet) + tm_lines() +
+  tm_shape(centroids_quiet_5km) + tm_bubbles("n_students")
+tm_shape(top_routes_fast) + tm_lines() +
+  tm_shape(centroids_fast_5km) + tm_bubbles("n_students")
+
+# Bubbles by Go Dutch uptake
+# m4 = tm_shape(centroids_quiet_5km |> rename(`Potential cyclists` = bicycle_godutch)) + tm_bubbles("Potential cyclists", alpha = 0.3) + 
+#   tm_shape(school) + tm_bubbles(col = "green") +
+#   tm_shape(top_routes_quiet) + tm_lines(lwd = 2)
+# m8 = tm_shape(centroids_fast_5km |> rename(`Potential cyclists` = bicycle_godutch)) + tm_bubbles("Potential cyclists", alpha = 0.3) + 
+#   tm_shape(school) + tm_bubbles(col = "green") +
+#   tm_shape(top_routes_fast) + tm_lines(lwd = 2)
+
+# Could also add feature to function so routes are penalised if students live far away from the route origin?
+
+tmap_arrange(m9, m10)
+
+
 
