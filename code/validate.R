9f121ce11c3787afa3e2b0f5bc4630e05b3d57d3
joeytalbot <joeytalbot85@gmail.com>
2025-03-17 18:03:55 +0000

    buffer

--- code/validate.R
+++ code/validate.R
@@ -5,3 +5,7 @@ tm_shape(comboios) + tm_lines("name", lwd = 2)
 # Could also add data for extra routes in Almada, etc
 
 # Make a buffer around each line and compare the centroids within the buffer to the centroids within an equivalent buffer around the candidate bike bus routes
+buffer = 200
+comboios_buff = stplanr::geo_buffer(comboios, dist = buffer)
+
+tm_shape(comboios_buff) + tm_polygons("name", alpha = 0.5)
