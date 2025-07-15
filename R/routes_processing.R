a6bbd45e4f865b392ff883f65885fddb671891db
Juan P. Fonseca-Zamora <69847296+juanfonsecaLS1@users.noreply.github.com>
2025-07-15 12:52:29 +0100

    improved coding of filter

--- R/routes_processing.R
+++ R/routes_processing.R
@@ -23,20 +23,24 @@ filter_routes = function(routes,
   
   attribute_trips <- match.arg(attribute_trips)
   
-  z = nrow(routes) - 1
-  
   attribute_trips_x_distance = paste0(attribute_trips, "_x_distance")
   
   routes_sorted <- routes[order(routes[[attribute_trips_x_distance]],decreasing = FALSE), ]
   
+  route_names <- names(routes_sorted)[grep("geom",x = names(routes_sorted),invert = T)]
+  
   selected_routes = numeric(0)
   
-  for (i in 1:(nrow(routes) - 1)) {
-    route_i = routes[i, ]
-    p = st_cast(route_i$geometry, "POINT")
-    p1 = p[1]
-    better_routes = routes |> 
-      slice_tail(n = nrow(routes) - i)
+  routes_sorted_initial_point <- routes_sorted |>
+    sf::st_cast( "POINT") |> 
+    dplyr::slice_head(n = 1,by = dplyr::all_of(route_names))
+  
+  
+  
+  for (i in 1:(nrow(routes_sorted) - 1)) {
+    p1 = routes_sorted_initial_point[i,]
+    better_routes = routes_sorted |> 
+      dplyr::slice_tail(n = nrow(routes_sorted) - i)
     better_routes_union = sf::st_union(better_routes)
     distance = units::drop_units(sf::st_distance(p1, better_routes_union))
     if (distance > buffer) {
@@ -45,9 +49,9 @@ filter_routes = function(routes,
     
   }
   routes_subset = routes[selected_routes, ]
-  routes_subset = routes_subset[order(-routes_subset[["bicycle_godutch_x_distance"]]), ]
+  routes_subset = routes_subset[order(-routes_subset[[attribute_trips_x_distance]]), ]
   routes_subset = routes_subset |>
-    slice_head(n = top_n)
+    dplyr::slice_head(n = top_n)
   routes_subset
 }
 
